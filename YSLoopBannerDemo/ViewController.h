9b21b64502168a96a1e57f7b471e5a5976d96a87
zys <zys@zysdeiMac.local>
2016-10-18 11:57:28 +0800

    Initial Commit

---
+++ YSLoopBannerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YSLoopBannerDemo
+//
+//  Created by zys on 2016/10/18.
+//  Copyright © 2016年 张永帅. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YSLoopBannerDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  YSLoopBannerDemo
+//
+//  Created by zys on 2016/10/18.
+//  Copyright © 2016年 张永帅. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
